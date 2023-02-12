99cb302353922cb06438c6ea683df616ee5e0011
TayDeveloper <108765871+TayDeveloper@users.noreply.github.com>
2023-02-12 15:54:06 -0300

    Initial commit

---
+++ Tentandodenovo/IDEIASDATAY.py
@@ -0,0 +1,39 @@
+
+#toda mensagem tem que ter aspas
+print("ola mundo")
+
+#numeros não precisam de aspas
+print(7+4)
+
+#se colocar aspas entre os números eles cocatenam
+
+print('7'+'4')
+
+#se colocar aspas entre os números eles cocatenam,
+#Quando forem dados diferentes exemplo número e letra, ESPAÇO virgula.
+
+print('Ola Tay' , "4")
+
+#em python tudo é letra minuscula, em python toda variável
+# é um objeto e pode receber valores
+
+
+# ( = ) -> variável recebeu o valor de
+
+nome = 'Tayná'
+idade = 24
+peso = 70.0
+
+#nao precisa de "ASPAS" ABAIXO,pois estou citando as variáveis não o que vem após delas.
+print(nome, idade, peso)
+
+#fazendo perguntas interativas adcionaar a função "INPUT"
+#INPUT - LEIA
+#PRINT - ESCREVA
+
+nome = input('Qual seu nome? ') #colocar espaço na aspa para ESPAÇO
+idade = input("Quantos ano ce tem? ")
+peso = input("Quanto você pesa? ")
+
+#nao precisa de "ASPAS"
+print(nome, idade, peso)
